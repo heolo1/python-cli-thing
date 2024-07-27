6e1caeb006853143ddbfe4ff1cf914f71cd80c10
heolo1 <eldon15563+github@gmail.com>
2024-07-27 14:07:56 -0700

    add example commands

--- example.py
+++ example.py
@@ -1,4 +1,29 @@
-import command
+import command, random
+
+def on_load() -> bool:
+	global value1
+	value1 = random.randint(1, 100)
+	print("Set value")
+	return True
+
+@command.register(["example", "ex"], on_load=on_load)
+@command.desc("An example command.", "An example command. Prints out all arguments supplied to it.")
+def example(*args):
+	print(f"# Args: {len(args)}")
+	for i in range(len(args)):
+		print(f"Arg {i} - {args[i]}")
+
+def on_load2() -> bool:
+	global value2
+	value2 = random.randint(1, 100)
+	print("Set value2")
+	return True
+
+@command.register(parent=example, on_load=on_load2)
+@command.desc("Prints the numbers generated.", "Prints the numbers randomly generated on load.")
+def value():
+	print(f"value1: {value1}")
+	print(f"value2: {value2}")
 
 def main():
 	command.main()
